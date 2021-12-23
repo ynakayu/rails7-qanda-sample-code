ab44e0a044f6f8138819bcfaa85097586530f81b
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-12-23 05:32:44 +0000

    05-05-create-questions-controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,29 @@
+class QuestionsController < ApplicationController
+  # 質問一覧表示
+  def index
+  end
+  
+  # 質問詳細ページ表示
+  def show
+  end
+  
+  # 質問の作成
+  def new
+  end
+  
+  # 質問の登録
+  def create
+  end
+  
+  # 質問の編集
+  def edit
+  end
+  
+  # 質問の更新
+  def update
+  end
+  
+  # 質問の削除
+  def destroy
+  end
+end
