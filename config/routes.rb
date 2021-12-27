9c9da8c335066cb8af366c983a7d05ce1e3a5935
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-12-27 04:49:01 +0000

    05-19-answer-routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "questions#index"
-  resources :questions
+  resources :questions do
+    resources :answers
+  end
 end
