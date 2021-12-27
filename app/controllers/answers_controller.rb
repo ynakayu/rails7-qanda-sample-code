eae7a8766e8a12c2a7ba731373159e530b67fbb9
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-12-27 05:19:40 +0000

    05-21-post-answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,18 @@
 class AnswersController < ApplicationController
   # 回答の登録
   def create
+    # p params
+    @question = Question.find(params[:question_id])
+    @question.answers.create(answer_params)
+    redirect_to question_path(@question)
   end
   
   # 回答の削除
   def destroy
+  end
+  
+  private
+  def answer_params
+    params.require(:answer).permit(:name, :content)
   end
 end
