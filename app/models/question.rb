f9e57835a2c14b4f4dfd57054c48124dc3e1e46a
ynakayu <9111754+ynakayu@users.noreply.github.com>
2021-12-27 04:38:57 +0000

    05-18_answer-function

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ApplicationRecord
+  has_many :answers, dependent: :destroy
+  
   validates :title, presence: true
   validates :name, presence: true
   validates :content, presence: true, length: { minimum: 5 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_054439) do
+ActiveRecord::Schema.define(version: 2021_12_27_021734) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "name"
@@ -20,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_12_23_054439) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
 end
